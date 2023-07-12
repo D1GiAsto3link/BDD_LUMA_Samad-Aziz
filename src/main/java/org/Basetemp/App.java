dc1cfcdd02c82028c0f7b712c1f5b87a933a6680
D1GiAsto3link <Samad@digilink.africa>
2023-07-12 10:36:25 +0200

    New proj

---
+++ src/main/java/org/Basetemp/App.java
@@ -0,0 +1,13 @@
+package org.Basetemp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
